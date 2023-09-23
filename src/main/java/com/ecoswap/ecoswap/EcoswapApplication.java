6db2b1fd1f21a051c344154958924355c3bebdf7
Joao Henrique <joaoadsistemas@gmail.com>
2023-09-23 12:38:16 -0300

    Branch joao

--- src/main/java/com/ecoswap/ecoswap/EcoswapApplication.java
+++ src/main/java/com/ecoswap/ecoswap/EcoswapApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcoswapApplication {
 
+	//
 	///
 	public static void main(String[] args) {
 		SpringApplication.run(EcoswapApplication.class, args);
