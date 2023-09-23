d77b882d2860aad01d929a66b9b4c90b3f5d4148
Joao Henrique <joaoadsistemas@gmail.com>
2023-09-23 11:57:06 -0300

    Teste

---
+++ src/main/java/com/ecoswap/ecoswap/EcoswapApplication.java
@@ -0,0 +1,13 @@
+package com.ecoswap.ecoswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoswap/ecoswap/EcoswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoswap.ecoswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
