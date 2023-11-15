72c59914d4de7aeea6d6919790c9e75d396f76f4
PedroTejon <pedrohtejon@gmail.com>
2023-11-15 20:13:55 -0300

    add: Menus de Categoria integrados com Backend

--- EcoSwapFrontEnd/src/app/View/Model/cards/cards.component.ts
+++ EcoSwapFrontEnd/src/app/View/Model/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
+  @Input() produto!: any;
+  rating!: number;
 
+  counter(i: number) {
+    if (i != 0)
+      return new Array(i);
+    else 
+      return []
+  }
+
+  ngOnInit() {
+   
+    fetch("http://localhost:8080/api/avaliacao/mediaTrocas/" + this.produto.usuario.id_usuario, {
+        method: 'GET',
+        headers: { 
+          'Accept': 'application/json', 
+          'Content-Type': 'application/json', 
+          'Authorization': `${localStorage.getItem('token')}` 
+        },
+      })
+      .then(response => response.json())
+      .then((data) => {
+        this.rating = data.resultado;
+        console.log(data)
+      })
+  }
 }
