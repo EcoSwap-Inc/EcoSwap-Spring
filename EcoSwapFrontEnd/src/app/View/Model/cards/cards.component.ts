7ecac7af3d65a92209b12ceeb73fe821360b0d72
PedroTejon <pedrohtejon@gmail.com>
2023-11-16 01:37:46 -0300

    refactor: Menus de categoria

--- EcoSwapFrontEnd/src/app/View/Model/cards/cards.component.ts
+++ EcoSwapFrontEnd/src/app/View/Model/cards/cards.component.ts
@@ -3,33 +3,27 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
 })
 export class CardsComponent {
   @Input() produto!: any;
   rating!: number;
-
-  counter(i: number) {
-    if (i != 0)
-      return new Array(i);
-    else 
-      return []
-  }
+  imagem!: string;
 
   ngOnInit() {
    
     fetch("http://localhost:8080/api/avaliacao/mediaTrocas/" + this.produto.usuario.id_usuario, {
-        method: 'GET',
-        headers: { 
-          'Accept': 'application/json', 
-          'Content-Type': 'application/json', 
-          'Authorization': `${localStorage.getItem('token')}` 
-        },
-      })
-      .then(response => response.json())
-      .then((data) => {
-        this.rating = data.resultado;
-        console.log(data)
-      })
+      method: 'GET',
+      headers: { 
+        'Accept': 'application/json', 
+        'Content-Type': 'application/json',
+        'Authorization': `${localStorage.getItem('token')}` 
+      },
+    })
+    .then(response => response.json())
+    .then((data) => {
+      this.rating = data.resultado;
+      console.log(data)
+    })
   }
 }
