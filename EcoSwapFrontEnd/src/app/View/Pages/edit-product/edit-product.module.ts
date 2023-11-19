84a2df3487e18fa07938b4215fd7be84982565f2
Ana Moreira <anagiulialima06@gmail.com>
2023-11-19 13:12:40 -0300

    edit e inventory sla

--- EcoSwapFrontEnd/src/app/View/Pages/edit-product/edit-product.module.ts
+++ EcoSwapFrontEnd/src/app/View/Pages/edit-product/edit-product.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditProductRoutingModule } from './edit-product-routing.module';
 import { EditProductComponent } from './edit-product.component';
 
